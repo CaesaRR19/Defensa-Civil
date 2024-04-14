0af379781125c0b56a3055263703d3d68074242a
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2024-04-14 18:53:45 -0400

    fixing deprated

--- defensa_civil/lib/pages/videos/videos_body.dart
+++ defensa_civil/lib/pages/videos/videos_body.dart
@@ -113,9 +113,9 @@ class _VideosBodyState extends State<VideosBody> {
     );
   }
 
-  Future<void> _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
+  Future<void> launchURL(String url) async {
+    if (await canLaunchUrl(url as Uri)) {
+      await launchUrl(url as Uri);
     } else {
       throw 'Could not launch $url';
     }
